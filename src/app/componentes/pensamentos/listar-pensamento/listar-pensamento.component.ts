947b6e7001b0f033b668c60e313cc6ee50a457ec
Willyam Mach <willyam.mach@gmail.com>
2024-07-09 14:01:42 -0300

    utilizando ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -23,6 +23,16 @@ export class ListarPensamentoComponent implements OnInit {
       autoria: 'roger',
       modelo: 'modelo3'
     },
+    {
+      conteudo: "I love PHP",
+      autoria: 'roger',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: "I love make web sites and discover new technologys. I was studing php, but i have to know kotlin, so bye bye php, see you soon my brother. Kotlin is very interesting, cause we can use it to make web aplications on android ambient. it will be funny undertend this!",
+      autoria: 'Willyam',
+      modelo: 'modelo3'
+    },
   ];
   constructor() { }
 
