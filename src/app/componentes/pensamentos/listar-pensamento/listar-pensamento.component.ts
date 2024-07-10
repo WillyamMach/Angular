b004093bdb8f05b0d687052ee66137e36e329c00
Willyam Mach <willyam.mach@gmail.com>
2024-07-10 13:30:08 -0300

    add type

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,32 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: "I love Js",
-      autoria: 'Jonas',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: "I love jQuery",
-      autoria: 'Mathias',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: "I love PHP",
-      autoria: 'roger',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: "I love PHP",
-      autoria: 'roger',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: "I love make web sites and discover new technologys. I was studing php, but i have to know kotlin, so bye bye php, see you soon my brother. Kotlin is very interesting, cause we can use it to make web aplications on android ambient. it will be funny undertend this!",
-      autoria: 'Willyam',
-      modelo: 'modelo3'
-    },
+  listaPensamentos: Pensamento[] = [
+
   ];
   constructor() { }
 
