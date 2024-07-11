aae8d6a7add8bff9ad65724df214b9afe5d1f44b
Willyam Mach <willyam.mach@gmail.com>
2024-07-11 13:58:22 -0300

    start to work with delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     RodapeComponent,
     CriarPensamentoComponent,
     ListarPensamentoComponent,
-    PensamentoComponent,
+    PensamentoComponent
   ],
   imports: [
     BrowserModule,
