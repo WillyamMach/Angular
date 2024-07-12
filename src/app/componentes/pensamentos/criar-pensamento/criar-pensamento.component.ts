9f0e53d055448d552f2a1e9ab32986fbcf17f6fd
Willyam Mach <willyam.mach@gmail.com>
2024-07-12 13:38:53 -0300

    add delete option

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class CriarPensamentoComponent implements OnInit {
 
   pensamento: Pensamento = {
+    id: 1,
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
     modelo: 'modelo1'
